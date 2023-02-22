fb799115d5df2994c802e4d6df7806444d986769
Caio Jardim <caiojardinho@gmail.com>
2023-02-22 06:49:12 -0300

    Created new component to extract TaskList functionality out of componente App and start to implement the save and read functionality using localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,15 @@
 import './global.css';
 import { Header } from './components/Header'
-import { Task } from './components/Task'
+import { TaskList } from './components/TaskList'
 
-import  clipboard  from './assets/clipboard.svg'
 import styles from './App.module.css'
-import { PlusCircle } from 'phosphor-react';
+
 
 function App() {
   return (
     <>
       <Header />
-      <div className={styles.appContainer}>
-
-        <div className={styles.newTask}>
-          <input type="text" placeholder='Adicione uma nova tarefa' />
-          <button>Criar <PlusCircle/></button>
-        </div>
-
-        <div className={styles.taskStatus}>
-          <strong className={styles.colorBlue}>Tarefas criadas <span>5</span></strong>
-          <strong className={styles.colorPurple}>Concluídas <span>2 de 5</span></strong>
-        </div>
-
-        <div className={styles.tasks}>
-          {/* <div className={styles.withoutTasks}>
-            <img src={clipboard} alt="Clipboard Icon" />
-            <strong>Você ainda não tem tarefas cadastradas</strong>
-            <span>Crie tarefas e organize seus itens a fazer</span>
-          </div> */}
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-          <Task />
-        </div>
-      </div>
+      <TaskList />
     </>
   )
 }
---
+++ src/components/TaskList.tsx
@@ -0,0 +1,65 @@
+import styles from './TaskList.module.css'
+import  clipboard  from '../assets/clipboard.svg'
+import { PlusCircle } from 'phosphor-react';
+import { useState, useEffect } from 'react';
+
+import { Task } from './Task'
+
+interface TaskType {
+  id: number,
+  content: string,
+  isChecked: boolean
+}
+
+export function TaskList () {
+  const [taskList, setTaskList] = useState<TaskType[]>([])
+
+  useEffect (() => {
+    let taskList = localStorage.getItem('TaskList') || '[]'
+    let taskListObject = JSON.parse(taskList)
+    setTaskList(taskListObject)
+  }, [])
+
+  localStorage.setItem ('TaskList', JSON.stringify([
+    {
+      id:1,
+      content: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+      isChecked: false
+    }
+  ]))
+
+  
+
+  return (
+    <div className={styles.taskListContainer}>
+
+        <div className={styles.newTask}>
+          <input type="text" placeholder='Adicione uma nova tarefa' />
+          <button>Criar <PlusCircle/></button>
+        </div>
+
+        <div className={styles.taskStatus}>
+          <strong className={styles.colorBlue}>Tarefas criadas <span>5</span></strong>
+          <strong className={styles.colorPurple}>Concluídas <span>2 de 5</span></strong>
+        </div>
+
+        <div className={styles.tasks}>
+          
+          {
+            (taskList.length !== 0 ) ?       
+              taskList.map ((task) => {
+                return <Task key={task.id} />
+              })
+              : 
+              <div className={styles.withoutTasks}>
+                <img src={clipboard} alt="Clipboard Icon" />
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                <span>Crie tarefas e organize seus itens a fazer</span>
+              </div> 
+          }
+          
+        </div>
+      </div>
+    
+  )
+}
